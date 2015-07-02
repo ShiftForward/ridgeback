c221d4d8cec07d96322b10aa35b7a4e1163e0b23
DDuarte <dnpd.dd@gmail.com>
2015-07-02 18:10:12 +0100

    Load http interface and port from config

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -3,6 +3,7 @@ import akka.io.IO
 import akka.pattern.ask
 import akka.util.Timeout
 import api.RoutesActor
+import com.typesafe.config.ConfigFactory
 import spray.can.Http
 import utils.{ActorModuleImpl, ConfigurationModuleImpl, PersistenceModuleImpl}
 
@@ -16,9 +17,11 @@ object Boot extends App   {
   // create and start our service actor
   val service = modules.system.actorOf(Props(classOf[RoutesActor], modules), "routesActor")
 
+  val config = ConfigFactory.load()
+
   implicit val system = modules.system
   implicit val timeout = Timeout(5.seconds)
 
   // start a new HTTP server on the configured port with our service actor as the handler
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
+  IO(Http) ? Http.Bind(service, interface = config.getString("app.interface"), port = config.getInt("app.port"))
 }
