bf9314094fe51bed9de8e09fa02942a76c5f9bdd
DDuarte <dnpd.dd@gmail.com>
2015-07-06 14:22:32 +0100

    Fix a warning in JsonProtocol.scala due to imports

--- src/main/scala/persistence/entities/JsonProtocol.scala
+++ src/main/scala/persistence/entities/JsonProtocol.scala
@@ -1,17 +1,11 @@
 package persistence.entities
 
-import persistence.entities.{Test, SimpleTest, SimpleProject, Project}
 import spray.json.DefaultJsonProtocol
 import java.sql.Timestamp
 
 import spray.json._
 
 object JsonProtocol extends DefaultJsonProtocol {
-  implicit val projectFormat = jsonFormat3(Project)
-  implicit val testFormat = jsonFormat5(Test)
-  implicit val simpleProjectFormat = jsonFormat2(SimpleProject)
-  implicit val simpleTestFormat = jsonFormat2(SimpleTest)
-
   implicit object TimestampJsonFormat extends JsonFormat[Timestamp] {
     def write(x: Timestamp) = JsNumber(x.getTime)
     def read(value: JsValue) = value match {
@@ -19,4 +13,9 @@ object JsonProtocol extends DefaultJsonProtocol {
       case x => deserializationError("Expected Timestamp as JsNumber, but got " + x)
     }
   }
+
+  implicit val projectFormat = jsonFormat3(Project)
+  implicit val testFormat = jsonFormat5(Test)
+  implicit val simpleProjectFormat = jsonFormat2(SimpleProject)
+  implicit val simpleTestFormat = jsonFormat2(SimpleTest)
 }
