a322d117ae92516197238bc4ecaa0e4c19d68b0f
DDuarte <dnpd.dd@gmail.com>
2015-07-15 22:57:41 +0100

    Unit tests for TestsDAL, JobsDAL and new routes

--- src/main/scala/persistence/entities/Test.scala
+++ src/main/scala/persistence/entities/Test.scala
@@ -24,10 +24,10 @@ trait Tests extends Profile {
     def id = column[Int]("TEST_ID", O.PrimaryKey, O.AutoInc)
     def projId = column[Int]("PROJ_ID")
     def commit = column[String]("COMMIT")
-    def startDate = column[Timestamp]("START_DATE")
+    def startDate = column[Option[Timestamp]]("START_DATE")
     def endDate = column[Option[Timestamp]]("END_DATE")
 
-    def * = (id.?, projId.?, commit, startDate.?, endDate) <> (Test.tupled, Test.unapply)
+    def * = (id.?, projId.?, commit, startDate, endDate) <> (Test.tupled, Test.unapply)
     // TODO def project = foreignKey("PROJ_FK", projId, TableQuery[Projects])(_.id)
   }
 
