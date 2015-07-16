f544ddeab2c44a571a9a09f836b8b8edda102ecf
DDuarte <dnpd.dd@gmail.com>
2015-07-16 10:40:50 +0100

    Fix build after merge

--- src/test/scala/api/RoutesSpec.scala
+++ src/test/scala/api/RoutesSpec.scala
@@ -1,15 +1,15 @@
 package api
 
-import java.sql.Timestamp
+import java.time.ZonedDateTime
 
-import persistence.entities.{ Test, JsonProtocol, SimpleProject, Project }
-import spray.httpx.SprayJsonSupport
+import persistence.entities.JsonProtocol._
+import persistence.entities.{JsonProtocol, Project, SimpleProject, Test}
+import spray.http.StatusCodes._
 import spray.http._
-import StatusCodes._
+import spray.httpx.SprayJsonSupport
+import spray.httpx.SprayJsonSupport._
+
 import scala.concurrent.Future
-import JsonProtocol._
-import SprayJsonSupport._
-import java.util.Date
 
 class RoutesSpec extends AbstractAPITest {
   sequential
@@ -81,7 +81,7 @@ class RoutesSpec extends AbstractAPITest {
 
     "trigger route runs successful" in {
       modules.projectsDal.getProjectById(2) returns Future(Some(Project(Some(2), "name 1", "url 1")))
-      modules.testsDal.save(Test(None, Some(2), "commit", Some(any[Timestamp]), None)) returns Future(3)
+      modules.testsDal.save(Test(None, Some(2), "commit", Some(any[ZonedDateTime]), None)) returns Future(3)
 
       Post("/project/2/trigger") ~> projects.ProjectTriggerRoute ~> check {
         handled must beTrue
