a67749f0f059abef6e873144ff2f7af145f4d8a7
DDuarte <dnpd.dd@gmail.com>
2015-07-27 13:57:49 +0100

    Simplify BitbucketPayloadJsonReader.read with a match

--- src/main/scala/core/PayloadJsonProtocol.scala
+++ src/main/scala/core/PayloadJsonProtocol.scala
@@ -7,32 +7,31 @@ import spray.json.{ DefaultJsonProtocol, _ }
 object PayloadJsonProtocol extends DefaultJsonProtocol {
   implicit object BitbucketPayloadJsonReader extends RootJsonReader[Payload] {
     def read(json: JsValue): Payload = {
+      (json.extract[Option[JsObject]]('pullrequest.?), json.extract[Option[JsObject]]('commit.?)) match {
+        case (Some(_), None) =>
+          val commitOpt = json.extract[Option[String]]('pullrequest / 'source / 'commit / 'hash)
+          val branchOpt = json.extract[Option[String]]('pullrequest / 'source / 'branch / 'name)
+          val prIdOpt = json.extract[Option[Int]]('comment / 'pullrequest / 'id)
+          val repoNameOpt = json.extract[Option[String]]('repository / 'full_name)
+          val commentOpt = json.extract[Option[String]]('comment / 'content / 'raw)
 
-      if (json.extract[Option[String]]('pullrequest.? / 'type).isDefined) {
-        val commitOpt = json.extract[Option[String]]('pullrequest / 'source / 'commit / 'hash)
-        val branchOpt = json.extract[Option[String]]('pullrequest / 'source / 'branch / 'name)
-        val prIdOpt = json.extract[Option[Int]]('comment / 'pullrequest / 'id)
-        val repoNameOpt = json.extract[Option[String]]('repository / 'full_name)
-        val commentOpt = json.extract[Option[String]]('comment / 'content / 'raw)
+          (commitOpt, branchOpt, prIdOpt, repoNameOpt, commentOpt) match {
+            case (Some(commit), Some(branch), Some(prId), Some(repoName), Some(comment)) =>
+              PullRequestPayload(comment, "bitbucket", repoName, commit, prId)
+            case _ => deserializationError(s"Could not extract PR fields from the PR Bitbucket payload: ${json.toString()}")
+          }
+        case (None, Some(_)) =>
+          val commitOpt = json.extract[Option[String]]('commit / 'hash)
+          val commentIdOpt = json.extract[Option[Int]]('comment / 'id)
+          val repoNameOpt = json.extract[Option[String]]('repository / 'full_name)
+          val commentOpt = json.extract[Option[String]]('comment / 'content / 'raw)
 
-        (commitOpt, branchOpt, prIdOpt, repoNameOpt, commentOpt) match {
-          case (Some(commit), Some(branch), Some(prId), Some(repoName), Some(comment)) =>
-            PullRequestPayload(comment, "bitbucket", repoName, commit, prId)
-          case _ => deserializationError(s"Could not extract PR fields from the PR Bitbucket payload: ${json.toString()}")
-        }
-      } else if (json.extract[Option[String]]('commit.? / 'type).isDefined) {
-        val commitOpt = json.extract[Option[String]]('commit / 'hash)
-        val commentIdOpt = json.extract[Option[Int]]('comment / 'id)
-        val repoNameOpt = json.extract[Option[String]]('repository / 'full_name)
-        val commentOpt = json.extract[Option[String]]('comment / 'content / 'raw)
-
-        (commitOpt, commentIdOpt, repoNameOpt, commentOpt) match {
-          case (Some(commit), Some(commentId), Some(repoName), Some(comment)) =>
-            CommitPayload(comment, "bitbucket", repoName, commit, commentId)
-          case _ => deserializationError(s"Could not extract commit fields from the commit Bitbucket payload: ${json.toString()}")
-        }
-      } else {
-        deserializationError(s"Bitbucket payload doesn't match anything known: ${json.toString()}")
+          (commitOpt, commentIdOpt, repoNameOpt, commentOpt) match {
+            case (Some(commit), Some(commentId), Some(repoName), Some(comment)) =>
+              CommitPayload(comment, "bitbucket", repoName, commit, commentId)
+            case _ => deserializationError(s"Could not extract commit fields from the commit Bitbucket payload: ${json.toString()}")
+          }
+        case _ => deserializationError(s"Bitbucket payload doesn't match anything known: ${json.toString()}")
       }
     }
   }
