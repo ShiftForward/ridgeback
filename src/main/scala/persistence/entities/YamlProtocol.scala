12679f790626d1248b36c0c0ff9735fad2925d59
Joao Azevedo <joao.c.azevedo@gmail.com>
2015-07-30 18:54:32 +0100

    Fix ordering of YamlFormat declarations

--- src/main/scala/persistence/entities/YamlProtocol.scala
+++ src/main/scala/persistence/entities/YamlProtocol.scala
@@ -3,6 +3,6 @@ package persistence.entities
 import net.jcazevedo.moultingyaml.DefaultYamlProtocol
 
 object YamlProtocol extends DefaultYamlProtocol {
-  implicit val testsConfigurationFormat = yamlFormat3(TestsConfiguration)
   implicit val jobDefinitionFormat = yamlFormat9(JobDefinition)
+  implicit val testsConfigurationFormat = yamlFormat3(TestsConfiguration)
 }
