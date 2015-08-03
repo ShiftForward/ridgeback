6b142fc1e329bc1e82c4b734214693fb7596cef0
DDuarte <dnpd.dd@gmail.com>
2015-08-03 14:26:50 +0100

    Integrate some parts of the bootstrap template

--- build.config.js
+++ build.config.js
@@ -62,13 +62,19 @@ module.exports = {
    */
   vendor_files: {
     js: [
+      'vendor/jquery/dist/jquery.js',
       'vendor/angular/angular.js',
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
-      'vendor/angular-ui-utils/modules/route/route.js'
+      'vendor/angular-ui-utils/modules/route/route.js',
+      'vendor/metisMenu/dist/metisMenu.js',
+      'vendor/raphael/raphael.js',
+      'vendor/morrisjs/morris.js'
     ],
     css: [
+      'vendor/metisMenu/dist/metisMenu.css',
+      'vendor/morrisjs/morris.css'
     ],
     assets: [
     ]
