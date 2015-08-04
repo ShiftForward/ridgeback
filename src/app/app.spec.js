6a681d9f8d3f425821a8dc64c670c3db183f3abf
DDuarte <dnpd.dd@gmail.com>
2015-08-04 12:36:57 +0100

    Format code in app.spec.js and dashboard files

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -1,17 +1,17 @@
-describe( 'AppCtrl', function() {
-  describe( 'isCurrentUrl', function() {
+describe('AppCtrl', function () {
+  describe('isCurrentUrl', function () {
     var AppCtrl, $location, $scope;
 
-    beforeEach( module( 'ngBoilerplate' ) );
+    beforeEach(module('ngBoilerplate'));
 
-    beforeEach( inject( function( $controller, _$location_, $rootScope ) {
+    beforeEach(inject(function ($controller, _$location_, $rootScope) {
       $location = _$location_;
       $scope = $rootScope.$new();
-      AppCtrl = $controller( 'AppCtrl', { $location: $location, $scope: $scope });
+      AppCtrl = $controller('AppCtrl', {$location: $location, $scope: $scope});
     }));
 
-    it( 'should pass a dummy test', inject( function() {
-      expect( AppCtrl ).toBeTruthy();
+    it('should pass a dummy test', inject(function () {
+      expect(AppCtrl).toBeTruthy();
     }));
   });
 });
